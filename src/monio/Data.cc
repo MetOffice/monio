e24201d83462d7119b09816118e5063f6e7a9c2d
phlndrwd <philjunderwood@gmail.com>
2023-06-27 17:07:06 +0100

    Updated copyright notice on source files.

--- src/monio/Data.cc
+++ src/monio/Data.cc
@@ -1,9 +1,11 @@
-/*
- * (C) Crown Copyright 2022 Met Office
- *
- * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
- */
+/*#############################################################################
+# MONIO - Met Office NetCDF Input Output                                      #
+#                                                                             #
+# (C) Crown Copyright 2023 Met Office                                         #
+#                                                                             #
+# This software is licensed under the terms of the Apache Licence Version 2.0 #
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.        #
+#############################################################################*/
 #include "Data.h"
 
 #include <stdexcept>
--- src/monio/File.cc
+++ src/monio/File.cc
@@ -1,10 +1,12 @@
 ﻿
-/*
- * (C) Crown Copyright 2022 Met Office
- *
- * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
- */
+/*#############################################################################
+# MONIO - Met Office NetCDF Input Output                                      #
+#                                                                             #
+# (C) Crown Copyright 2023 Met Office                                         #
+#                                                                             #
+# This software is licensed under the terms of the Apache Licence Version 2.0 #
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.        #
+#############################################################################*/
 #include "File.h"
 
 #include <map>
