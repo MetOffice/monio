0c235d60bcf0f6ed8aea6f668c0fd28590615aff
phlndrwd <philjunderwood@gmail.com>
2023-06-28 11:20:38 +0100

    Restructured read-write test and added full set of input data to corresponding YAML.

--- test/monio/MonioReadWrite.h
+++ test/monio/MonioReadWrite.h
@@ -36,12 +36,24 @@
 
 namespace monio {
 namespace test {
-void read() {
-  oops::Log::debug() << "readFile()" << std::endl;
+void init() {
+  oops::Log::info() << "monio::test::init()" << std::endl;
+}
+
+void readInput() {
+  oops::Log::info() << "monio::test::readInput()" << std::endl;
 }
 
 void write() {
-  oops::Log::debug() << "readFile()" << std::endl;
+  oops::Log::info() << "monio::test::write()" << std::endl;
+}
+
+void readOutput() {
+  oops::Log::info() << "monio::test::readOutput()" << std::endl;
+}
+
+void compare() {
+  oops::Log::info() << "monio::test::compare()" << std::endl;
 }
 
 class MonioReadWrite : public oops::Test{
@@ -56,13 +68,25 @@ class MonioReadWrite : public oops::Test{
   void register_tests() const override {
     std::vector<eckit::testing::Test>& ts = eckit::testing::specification();
 
-    std::function<void(std::string&, int&, int)> readFunction =
-        [](std::string &, int&, int) { read(); };
-    ts.push_back(eckit::testing::Test("monio/read", readFunction));
+    std::function<void(std::string&, int&, int)> initFunction =
+        [](std::string &, int&, int) { init(); };
+    ts.push_back(eckit::testing::Test("monio/init", initFunction));
+
+    std::function<void(std::string&, int&, int)> readInputFunction =
+        [](std::string &, int&, int) { readInput(); };
+    ts.push_back(eckit::testing::Test("monio/readInput", readInputFunction));
 
     std::function<void(std::string&, int&, int)> writeFunction =
-        [](std::string &, int&, int) { read(); };
+        [](std::string &, int&, int) { write(); };
     ts.push_back(eckit::testing::Test("monio/write", writeFunction));
+
+    std::function<void(std::string&, int&, int)> readOutputFunction =
+        [](std::string &, int&, int) { readOutput(); };
+    ts.push_back(eckit::testing::Test("monio/readOutput", readOutputFunction));
+
+    std::function<void(std::string&, int&, int)> compareFunction =
+        [](std::string &, int&, int) { compare(); };
+    ts.push_back(eckit::testing::Test("monio/compare", compareFunction));
   }
   void clear() const override {}
 };
