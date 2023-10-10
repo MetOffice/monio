b62211c500be37aeeafa34523f44ae70f6af8582
phlndrwd <philjunderwood@gmail.com>
2023-10-10 17:41:04 +0100

    Added updated headers to testing source files.

--- test/mains/TestStateFull.cc
+++ test/mains/TestStateFull.cc
@@ -1,11 +1,11 @@
-/*#############################################################################
-# MONIO - Met Office NetCDF Input Output                                      #
-#                                                                             #
-# (C) Crown Copyright 2023 Met Office                                         #
-#                                                                             #
-# This software is licensed under the terms of the Apache Licence Version 2.0 #
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.        #
-#############################################################################*/
+/******************************************************************************
+* MONIO - Met Office NetCDF Input Output                                      *
+*                                                                             *
+* (C) Crown Copyright 2023 Met Office                                         *
+*                                                                             *
+* This software is licensed under the terms of the Apache Licence Version 2.0 *
+* which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.        *
+******************************************************************************/
 #include "../monio/StateFull.h"
 #include "oops/runs/Run.h"
 
