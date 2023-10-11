6bd04da17eb64b40ab485c7c720a52d2d69fc380
phlndrwd <philjunderwood@gmail.com>
2023-10-11 17:07:31 +0100

    Added BSD 3-Clause license file and headers to all applicable files.

--- test/mains/TestFieldSetWrite.cc
+++ test/mains/TestFieldSetWrite.cc
@@ -1,17 +1,17 @@
 /******************************************************************************
 * MONIO - Met Office NetCDF Input Output                                      *
 *                                                                             *
-* (C) Crown Copyright 2023 Met Office                                         *
+* (C) Crown Copyright 2023, Met Office. All rights reserved.                  *
 *                                                                             *
-* This software is licensed under the terms of the Apache Licence Version 2.0 *
-* which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.        *
+* This software is licensed under the terms of the 3-Clause BSD License       *
+* which can be obtained from https://opensource.org/license/bsd-3-clause/.    *
 ******************************************************************************/
 #include "../monio/FieldSetWrite.h"
 #include "oops/runs/Run.h"
 
 /// \brief This test targets Monio::writeFieldSet. An LFRic background file is read, field sets are
 ///        populated, and written directly to file. No comparison takes place as the data are left
-///        in Atlas order. A test pass is achieved simply if the function calls pass without issue.
+///        in Atlas order. A pass is achieved simply if the function calls complete without issue.
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
   monio::test::FieldSetWrite tests;
--- test/mains/TestStateBasic.cc
+++ test/mains/TestStateBasic.cc
@@ -1,17 +1,17 @@
 /******************************************************************************
 * MONIO - Met Office NetCDF Input Output                                      *
 *                                                                             *
-* (C) Crown Copyright 2023 Met Office                                         *
+* (C) Crown Copyright 2023, Met Office. All rights reserved.                  *
 *                                                                             *
-* This software is licensed under the terms of the Apache Licence Version 2.0 *
-* which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.        *
+* This software is licensed under the terms of the 3-Clause BSD License       *
+* which can be obtained from https://opensource.org/license/bsd-3-clause/.    *
 ******************************************************************************/
 #include "../monio/StateBasic.h"
 #include "oops/runs/Run.h"
 
 /// \brief This test targets a workflow that avoids the Monio singleton, and the use of Atlas. It
 ///        reads an input file, writes the data to file, reads that back in and checks the data for
-///        integrity. A test pass is achieved if the data first read matches the written data.
+///        integrity. A test pass is achieved if the data first read matches that written.
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
   monio::test::StateBasic tests;
